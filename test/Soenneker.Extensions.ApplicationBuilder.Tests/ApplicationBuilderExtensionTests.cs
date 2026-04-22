463440a2cedf36187c247d91dbf4af900ed97983
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:48 -0500

    TUnit conversion

--- test/Soenneker.Extensions.ApplicationBuilder.Tests/ApplicationBuilderExtensionTests.cs
+++ test/Soenneker.Extensions.ApplicationBuilder.Tests/ApplicationBuilderExtensionTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Extensions.ApplicationBuilder.Tests;
 
 public sealed class ApplicationBuilderExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
